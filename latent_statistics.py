20721c0dd92ab4c4452e7a5dee414bae5db6038f
nynke <ntilkema@hotmail.com>
2024-05-13 14:37:50 +0200

    apical basal and permutation without gpu

--- latent_statistics.py
+++ latent_statistics.py
@@ -1,7 +1,8 @@
 import numpy as np
 import random
 from scipy.spatial.distance import cdist
-
+from math import factorial, comb
+from multiprocessing import Pool
 
 seed = 101
 random.seed(seed)
@@ -20,11 +21,27 @@ def chamfer_L1_distance(point_cloud_1, point_cloud_2):
     # Compute the Chamfer distance
     return np.mean(distances_1_to_2) + np.mean(distances_2_to_1)
 
-def permutation_test(pattern, control, n_permutations: int = 1000):
+
+def permutation_test(pattern, control, n_permutations: int = 9999):
     observed_statistic = chamfer_L1_distance(pattern, control)
     num_pattern = len(pattern)
     combined = np.concatenate([pattern, control])
-    
+    len_combined = len(combined)
+
+    # Count max number of permutations with Combination rule nCr, where r is the pattern size
+    if num_pattern < 15:
+        total_permutations = comb(len_combined, num_pattern) # built in implementation of nCr rule.
+
+        # Adjust n_permutations if it's larger than total_permutations
+        if n_permutations > total_permutations:
+            exact_test = True
+            n_permutations = int(total_permutations)
+        else:
+            exact_test = False
+    else:
+        # If num_pattern is 15, the total combinations are 1.5e8, which already is much larger than 9999. So we skip calculating the factorials for 15+ to save compute time. 
+        exact_test = False
+
     # Preallocate permuted null hypothesis array
     permuted_statistics = np.empty(n_permutations)
 
@@ -41,9 +58,10 @@ def permutation_test(pattern, control, n_permutations: int = 1000):
     gamma = np.abs(eps * observed_statistic)
     cmps_less = permuted_statistics <= observed_statistic + gamma
     cmps_greater = permuted_statistics >= observed_statistic - gamma
-    # +1 is added to pvalues to add the observed value into the hypothetical population to make the pvalue more conservative.
-    pvalues_less = (cmps_less.sum() + 1) / (n_permutations + 1)
-    pvalues_greater = (cmps_greater.sum() + 1) / (n_permutations + 1)
+    # +1 is added to pvalues to add the observed value into the hypothetical population to make the pvalue more conservative. If it is an exact test, will use the true pvalue.
+    adjustment = 0 if exact_test else 1
+    pvalues_less = (cmps_less.sum() + adjustment) / (n_permutations + adjustment)
+    pvalues_greater = (cmps_greater.sum() + adjustment) / (n_permutations + adjustment)
     #because with 2-tailed you should use alpha=0.025 as treshold, so now it gets scaled back to 0.05
     p_value = np.minimum(pvalues_less, pvalues_greater) * 2 
 
